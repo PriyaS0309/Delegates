53e30571a9f85774bc5a7ee15beea627f7760d4e
PriyaS0309 <109306451+PriyaS0309@users.noreply.github.com>
2022-07-26 18:28:20 +0530

    Add project files.

---
+++ MultipleDelegates/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultipleDelegates
+{
+    public delegate void show_delegate();
+    public delegate void Calculation(int a,int b);
+    class Program
+    {
+    
+        public static void show()
+        {
+            Console.WriteLine("Hello");
+        }
+        public static void add(int num1,int num2)
+        {
+            int result = num1 + num2;
+            Console.WriteLine("Addition is:{0}",result);
+        }
+        static void Main(string[] args)
+        {
+            show_delegate sd = new show_delegate(show);
+            sd();
+
+            Calculation cl = new Calculation(add);
+            cl(10,30);
+
+        }
+    }
+}
